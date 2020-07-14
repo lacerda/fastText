533bb80793a67824f9f194b795c4337e30baefd9
Rafael Lacerda <lacerda@users.noreply.github.com>
2020-07-14 12:10:27 -0400

    Increase MAX_VOCAB_SIZE

--- src/dictionary.h
+++ src/dictionary.h
@@ -33,7 +33,7 @@ struct entry {
 
 class Dictionary {
  protected:
-  static const int32_t MAX_VOCAB_SIZE = 30000000;
+  static const int32_t MAX_VOCAB_SIZE = 150000000;
   static const int32_t MAX_LINE_SIZE = 1024;
 
   int32_t find(const std::string&) const;
